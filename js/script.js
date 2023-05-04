efb4810f96455d534c2a703e7529a1b9c9fd15e2
Alekstok97 <alekstok97@mail.ru>
2023-05-04 10:00:07 +0600

    add price

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-document.addEventListener('DOMContentLoaded',function(){
-
-    const popularItemWrap =  document.querySelector('.popular_item_wrapper')
-    popularItemWrap.addEventListener('click',(e)=>{
-        // console.log(e.target)
-        if(e.target.classList.contains('active-btn__blue')){
-            window.location.replace('cart.html');
-        }
-        // console.log(e.target.classList)
-        if(e.target.classList.contains('btn__blue')){
-            e.target.classList.remove('btn__blue');
-            e.target.classList.add('active-btn__blue');
-            // console.log(e.target.parentElement)
-            console.log(e.target)
-
-            // const link = document.createElement('a')
-            // link.setAttribute('href','cart.html')
-            // e.target.append(link);
-            
-            // console.log('containes')
-        }
-       
-        // active-btn__blue
-        // btn__blue
-    })
-    
-
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const popularItemWrap = document.querySelector(".popular_item_wrapper");
+  popularItemWrap.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn__blue")) {
+      const parentDiv = e.target.parentElement;
+      e.target.classList.remove("btn__blue");
+      e.target.classList.add("active-btn__blue");
+      const link = document.createElement("a");
+      link.setAttribute("href", "cart.html");
+      link.appendChild(e.target);
+      parentDiv.appendChild(link);
+    }
+  });
+});
