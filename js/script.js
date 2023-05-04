7a5217f9824f502f0fc50a247c29bfcbcdef009d
Alekstok97 <alekstok97@mail.ru>
2023-05-04 10:00:15 +0600

    add price

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const popularItemWrap = document.querySelector(".popular_item_wrapper");
   popularItemWrap.addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn__blue")) {
-      const parentDiv = e.target.parentElement;
-      e.target.classList.remove("btn__blue");
-      e.target.classList.add("active-btn__blue");
+    const btn = e.target;
+    if (btn.classList.contains("btn__blue")) {
+      const parentDiv = btn.parentElement;
+      btn.classList.remove("btn__blue");
+      btn.classList.add("active-btn__blue");
       const link = document.createElement("a");
       link.setAttribute("href", "cart.html");
-      link.appendChild(e.target);
+      link.appendChild(btn);
       parentDiv.appendChild(link);
     }
   });
