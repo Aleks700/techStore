7e334994013ef8ef87d54ce503e0444309a7e9c6
Alekstok97 <alekstok97@mail.ru>
2023-05-04 12:08:57 +0600

    localstorage

--- js/script.js
+++ js/script.js
@@ -10,6 +10,55 @@ document.addEventListener("DOMContentLoaded", function () {
       link.setAttribute("href", "cart.html");
       link.appendChild(btn);
       parentDiv.appendChild(link);
+      const price = parentDiv.querySelector(".price span").innerText;
+      const productName = parentDiv.querySelector(".product_name").innerText;
+      const imgSrc = parentDiv.querySelector("img").src;
+      const techStore = localStorage.getItem("tech_store");
+      console.log(techStore);
+      if (techStore !== null) {
+        const parsed = JSON.parse(techStore);
+        const findElement = parsed.findIndex(
+          (elem) => elem.productName === productName
+        );
+
+        if (findElement === -1) {
+          console.log(-1);
+          localStorage.setItem(
+            "tech_store",
+            JSON.stringify(
+              parsed.push({
+                productName: productName,
+                imgSrc: imgSrc,
+                price: price,
+                count: 1,
+              })
+            )
+          );
+        } else {
+          console.log(parsed[findElement].count);
+        }
+
+        // console.log(findElement);
+        // console.log(parsed[findElement]);
+        // console.log;
+      } else {
+        localStorage.setItem(
+          "tech_store",
+          JSON.stringify([
+            {
+              productName: productName,
+              imgSrc: imgSrc,
+              price: price,
+              count: 1,
+            },
+          ])
+        );
+        console.log(techStore);
+      }
+
+      console.log(price);
+      console.log(productName);
+      console.log(imgSrc);
     }
   });
 });
