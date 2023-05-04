f77e89fd8be715390d26ff45784a07679026839d
Alekstok97 <alekstok97@mail.ru>
2023-05-04 17:43:13 +0600

    before loop

--- js/cart.js
+++ js/cart.js
@@ -2,9 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("cart");
   const cartBlock = document.querySelector(".shopping-cart");
   const techStore = JSON.parse(localStorage.getItem("tech_store"));
+  if (techStore.length > 0) {
+    const { count, imgSrc, price, productName } = techStore[0];
+    console.log("count", count);
+    console.log("imgSrc", imgSrc);
+    console.log("price", price);
+    console.log("productName", productName);
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("shopping-cart__item");
+    console.log(cartItem);
+
+    cartItem.innerHTML = `  
+                                  <div class="shopping-cart__item_preview">
+                                      <img src=${imgSrc} alt="" />
+                                  </div>
+                                  <div class="shopping-cart__text">
+                                      <p>${productName}</p>
+                                      <p>Цена: <span class="bold">${price}</span></p>
+                                  </div>
+                                  <div class="counter-wrapper">
+                                      <button>-</button>
+                                      <button class="counter">${count}</button>
+                                      <button>+</button>
+                                  </div>
+                               `;
+    cartBlock.appendChild(cartItem);
+  }
   console.log(cartBlock);
   console.log(techStore);
-  const cartItem = document.createElement("div");
-  console.log(cartItem);
-  cartItem.innerHTML = ` <img>`;
 });
